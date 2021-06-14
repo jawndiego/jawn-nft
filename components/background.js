6d044ba47e031c97bdb954d9977f238713b9cd31
jawn <62079777+jawndiego@users.noreply.github.com>
2021-06-14 18:45:37 -0400

    Revert "First"
    This reverts commit 6507ce53233943617ca7cdcb230bd5631ce78947.

--- components/background.js
+++
@@ -1,6 +0,0 @@
-import React, { useEffect, useRef } from 'react'
-
-import Stroke from '../public/svg/brush-stroke.svg'
-
-
-    
--- scripts/randomColor.js
+++
@@ -1,2 +0,0 @@
-export default () =>
-  '#' + ('000000' + (Math.floor(Math.random() * 16777215).toString(16))).slice(-6)
